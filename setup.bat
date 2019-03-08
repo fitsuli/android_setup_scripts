6b813ab5f11bb2e8ec4b6970ad0c159877722fc5
fitsuli <danilyuk_2011@bk.ru>
2019-03-08 23:28:37 +0500

    Rearrange some items

--- setup.bat
+++ setup.bat
@@ -1,21 +1,24 @@
 @ECHO OFF
-adb shell settings put global airplane_mode_radios cell,nfc,wimax
+
 adb shell settings put global package_verifier_enable 0
-adb shell settings put global wifi_networks_available_notification_on 0
-adb shell settings put secure double_tap_to_wake 0
-adb shell settings put secure screensaver_enabled 0
 adb shell settings put secure user_setup_complete 1
-adb shell settings put secure tts_default_synth com.google.android.tts
 adb shell settings put secure user_full_data_backup_aware 1
-adb shell settings put secure sysui_qs_tiles wifi,cell,airplane,rotation,flashlight,bt,location,caffeine,screenrecord
-adb shell settings put global development_settings_enabled 1
+adb shell settings put global wifi_on 1
+adb shell settings put global wifi_networks_available_notification_on 0
 adb shell settings put global wifi_scan_always_enabled 1
 adb shell settings put global ble_scan_always_enabled 1
+
+adb shell settings put secure double_tap_to_wake 0
+adb shell settings put secure screensaver_enabled 0
+adb shell settings put secure tts_default_synth com.google.android.tts
+adb shell settings put global development_settings_enabled 1
 adb shell settings put global charging_sounds_enabled 0
-adb shell settings put global wifi_on 1
 adb shell settings put secure adb_notify 0
 adb shell settings put secure spell_checker_enabled 0
 adb shell settings put secure long_press_timeout 350
+
+adb shell settings put global airplane_mode_radios cell,nfc
+adb shell settings put secure sysui_qs_tiles wifi,cell,airplane,rotation,flashlight,bt,location,caffeine,screenrecord
 adb shell settings put secure icon_blacklist mobile,rotate,nfc,headset
 
 :: Disable default print service
