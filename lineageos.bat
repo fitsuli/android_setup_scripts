41973b5c0957768550e770af0074f959df86cac5
fitsuli <danilyuk_2011@bk.ru>
2019-03-08 20:11:03 +0500

    Add centered clock setting

--- lineageos.bat
+++ lineageos.bat
@@ -9,6 +9,7 @@ adb shell su -c settings put --lineage system system_profiles_enabled 0
 adb shell su -c settings put --lineage system torch_long_press_power_gesture 1
 adb shell su -c settings put --lineage system torch_long_press_power_timeout 300
 adb shell su -c settings put --lineage system trust_interface_hinted 1
+adb shell su -c settings put --lineage system status_bar_clock 1
 adb shell su -c settings put --lineage secure adb_notify 0
 adb shell su -c settings put --lineage secure advanced_reboot 1
 
