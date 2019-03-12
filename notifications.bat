3ea0fb81dd3999445f9e372d08ecf191e27a9e32
fitsuli <danilyuk_2011@bk.ru>
2019-03-12 20:13:45 +0500

    Add a few other notification things

--- notifications.bat
+++ notifications.bat
@@ -7,6 +7,20 @@ adb shell su -c sed -i '/Titanium/{s/importance="3/importance="1/}' /data/system
 :: Probably would interfere with something, but works okay on my device
 adb shell su -c sed -i '/Service/{s/importance="3/importance="1/}' /data/system/notification_policy.xml
 
+:: Disable Brave/Chrome Sites notifications
+adb shell su -c sed -i '/Sites/{s/blocked="false/blocked="true/}' /data/system/notification_policy.xml
+
+:: Disable Brave/Chrome Media notifications
+adb shell su -c sed -i '/Media/{s/importance="3/importance="0/}' /data/system/notification_policy.xml
+
+:: IME notification -> lowest priority
+adb shell su -c sed -i '/VIRTUAL_KEYBOARD/{s/importance="2/importance="1/}' /data/system/notification_policy.xml
+
+:: Developer notification -> lowest priority
+adb shell su -c sed -i '/DEVELOPER/{s/importance="2/importance="1/}' /data/system/notification_policy.xml
+
+:: Disable MacroDroid's persistent notification
+adb shell su -c sed -i '/persistent_notification/{s/importance="2/importance="0/}' /data/system/notification_policy.xml
 
 
 :: Reboot IMMEDIATELY in order to save all of the changes
