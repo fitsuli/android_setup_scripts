b1789b54ed25bafe741bd0f1721a4f73a79e71f9
fitsuli <danilyuk_2011@bk.ru>
2019-05-13 20:57:27 +0500

    Add the updated afterinstall.sh script Some stuff from the setup.sh moved here And I've added Tasker to the Doze whitelist

---
+++ afterinstall.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Fluid NG
+# adb shell settings put secure accessibility_enabled 1
+# adb shell settings put secure enabled_accessibility_services com.fb.fluid/com.fb.fluid.MainAccessibilityService
+
+# Notify & Fitness for Mi Band notification listener
+# adb shell su --preserve-environment -c "settings set secure enabled_notification_listeners com.mc.miband1/com.mc.miband1.NotificationService50"
+
+# adb shell settings put secure tts_default_synth com.google.android.tts
+
+# Disable AOSP keyboard
+adb shell ime disable com.android.inputmethod.latin/.LatinIME
+
+# Clear Card Emulator app's data
+# adb shell pm clear --user 0 com.yuanwofei.cardemulator.pro
+# adb shell pm grant --user 0 com.yuanwofei.cardemulator.pro android.permission.WRITE_EXTERNAL_STORAGE
+# adb shell pm grant --user 0 com.yuanwofei.cardemulator.pro android.permission.READ_EXTERNAL_STORAGE
+
+# Add a few apps to the Doze whitelist
+adb shell dumpsys deviceidle whitelist +com.topjohnwu.magisk
+adb shell dumpsys deviceidle whitelist +net.dinglisch.android.taskerm
+adb shell dumpsys deviceidle whitelist +com.arlosoft.macrodroid
+adb shell dumpsys deviceidle whitelist +com.mc.miband1
+adb shell dumpsys deviceidle whitelist +com.android.deskclock
+adb shell dumpsys deviceidle whitelist +com.google.android.deskclock
+adb shell dumpsys deviceidle whitelist +com.google.android.GoogleCamera
+adb shell dumpsys deviceidle whitelist +at.bitfire.davdroid
+
+# Revoke most Google Play permissions
+adb shell pm revoke --user 0 com.android.vending android.permission.ACCESS_COARSE_LOCATION
+adb shell pm revoke --user 0 com.android.vending android.permission.SEND_SMS
+adb shell pm revoke --user 0 com.android.vending android.permission.RECEIVE_SMS
+adb shell pm revoke --user 0 com.android.vending android.permission.READ_SMS
+adb shell pm revoke --user 0 com.android.vending android.permission.READ_CONTACTS
+
+# Grant Storage permissions to Magisk Manager
+adb shell pm grant --user 0 com.topjohnwu.magisk android.permission.WRITE_EXTERNAL_STORAGE
+adb shell pm grant --user 0 com.topjohnwu.magisk android.permission.READ_EXTERNAL_STORAGE
+
+echo Done
